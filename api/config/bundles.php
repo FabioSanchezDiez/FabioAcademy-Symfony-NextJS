63e7a872ac17669a8c3dac8877075f3471715811
Fabio <fabiodev3@gmail.com>
2024-05-04 18:47:30 +0200

    API | chore: CORS filter configuration

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -8,4 +8,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     AutoMapperPlus\AutoMapperPlusBundle\AutoMapperPlusBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
