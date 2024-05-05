2ee23d6e6d16d1770e3ac514b2cf312e9383c1d6
Fabio <fabiodev3@gmail.com>
2024-05-05 18:53:41 +0200

    FRONTEND | fonts: inter font was added

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import { inter } from "@/src/fonts";
 
 export const metadata: Metadata = {
   title: "FabioCode - Academy",
@@ -16,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
