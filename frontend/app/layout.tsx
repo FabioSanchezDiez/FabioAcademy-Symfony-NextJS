b2d91db6642b621b8e70c5e994519f8e5a2fa380
Fabio <fabiodev3@gmail.com>
2024-05-11 18:18:00 +0200

    FRONTEND | auth: next auth was installed and configured

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { inter } from "@/src/fonts";
 import NavBar from "@/components/navigation/NavBar";
-import { Providers } from "./providers";
+import { SessionAuthProvider, ThemeAppProvider } from "./providers";
 
 export const metadata: Metadata = {
   title: "FabioCode - Academy",
@@ -22,12 +22,14 @@ export default function RootLayout({
       <body
         className={`${inter.className} antialiased bg-gray-200 dark:bg-slate-900`}
       >
-        <Providers>
-          <header>
-            <NavBar></NavBar>
-          </header>
-          {children}
-        </Providers>
+        <ThemeAppProvider>
+          <SessionAuthProvider>
+            <header>
+              <NavBar></NavBar>
+            </header>
+            {children}
+          </SessionAuthProvider>
+        </ThemeAppProvider>
       </body>
     </html>
   );
--- frontend/app/providers.tsx
+++ frontend/app/providers.tsx
@@ -1,11 +1,20 @@
 "use client";
 
+import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
 
-export function Providers({ children }: { children: React.ReactNode }) {
+export function ThemeAppProvider({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       {children}
     </ThemeProvider>
   );
+}
+
+export function SessionAuthProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <SessionProvider>{children}</SessionProvider>;
 }
