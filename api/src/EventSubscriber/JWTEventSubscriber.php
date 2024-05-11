87f443ee68f54cc2fabeda59310a4c68759ead01
Fabio <fabiodev3@gmail.com>
2024-05-11 19:09:20 +0200

    API | jwt: jwt responses were improved

---
+++ api/src/EventSubscriber/JWTEventSubscriber.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use App\Entity\User;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationFailureEvent;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+class JWTEventSubscriber implements EventSubscriberInterface
+{
+    public function onAuthenticationSuccess(AuthenticationSuccessEvent $event): void
+    {
+        $user = $event->getUser();
+
+        if (!$user instanceof User) {
+            return;
+        }
+
+        $data = array_merge(
+            ["name" => $user->getName(), "email" => $user->getEmail()],
+            $event->getData()
+        );
+
+        $event->setData($data);
+    }
+
+    public function onAuthenticationFailure(AuthenticationFailureEvent $event): void
+    {
+        $code = Response::HTTP_UNAUTHORIZED;
+        $response = new JsonResponse([
+            "message" => "Las credenciales son incorrectas",
+            "code" => $code
+        ], $code);
+
+        $event->setResponse($response);
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            'lexik_jwt_authentication.on_authentication_success' => 'onAuthenticationSuccess',
+            'lexik_jwt_authentication.on_authentication_failure' => 'onAuthenticationFailure',
+        ];
+    }
+}
