b66713399ca34946172c0123e04076662d0ea7d7
Fabio <fabiodev3@gmail.com>
2024-05-19 17:23:44 +0200

    FRONTEND | ui: course page header was finished

--- frontend/components/ui/courses/CoursePage.tsx
+++ frontend/components/ui/courses/CoursePage.tsx
@@ -1,17 +1,17 @@
 import { Course } from "@/src/lib/definitions";
 import Image from "next/image";
 import Tag from "../elements/Tag";
-import TagYellow from "../elements/TagYellow";
-import { UserCircleIcon } from "@heroicons/react/24/outline";
+import { UserIcon } from "@heroicons/react/24/outline";
+import Button from "../elements/Button";
 
 export default function CoursePage({ course }: { course: Course }) {
   const publicationDate = new Date(course.publicationDate).toLocaleDateString();
   return (
-    <main className="flex gap-12 mt-12">
+    <main className="flex flex-col md:flex-row gap-12 mt-12">
       <section className="p-6">
         <h1 className="text-4xl font-black">{course.name}</h1>
         <p className="mt-6 text-lg">{course.description}</p>
-        <div className="flex gap-6 mt-8">
+        <div className="flex flex-col md:flex-row gap-6 mt-8">
           <Tag bgColor="bg-indigo-500">Categoría: {course.category}</Tag>
           <Tag bgColor="bg-fuchsia-500">
             Fecha de publicación: {publicationDate}
@@ -19,15 +19,22 @@ export default function CoursePage({ course }: { course: Course }) {
         </div>
       </section>
       <section>
-        <div className="p-4 bg-white dark:bg-slate-800 rounded-2xl shadow-2xl border-slate-600 hover:bg-slate-100 dark:hover:bg-slate-700">
+        <div className="bg-white dark:bg-slate-800 rounded-2xl shadow-2xl border-slate-600">
           <Image
             src={course.image}
             alt="Course Logo"
             width={700}
             height={700}
-            className="rounded-lg shadow-2xl"
           ></Image>
-          <div></div>
+          <div className="flex gap-2 items-center p-4">
+            <p className="text-md">Inscritos: {course.registeredUsers}</p>
+            <UserIcon className="w-4 h-4"></UserIcon>
+          </div>
+          <Button
+            textContent="Inscribirse"
+            textColor="text-white"
+            bgColor="bg-black"
+          ></Button>
         </div>
       </section>
     </main>
