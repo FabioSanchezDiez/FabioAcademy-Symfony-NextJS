7c6182d5b19273e5a8669e106433f24227665132
fabiosanchez-aircury <fabio.sanchez@aircury.com>
2024-05-27 11:57:50 +0200

    FRONTEND | ui: add hover to navigation links

--- frontend/components/navigation/NavLinks.tsx
+++ frontend/components/navigation/NavLinks.tsx
@@ -23,29 +23,39 @@ export default function NavLinks() {
   return (
     <>
       <Link href={"/"}>
-        <p className="dark:text-slate-400">Inicio</p>
+        <p className="dark:text-slate-400 dark:hover:text-slate-200 hover:text-gray-700">
+          Inicio
+        </p>
       </Link>
       <Link href={"/courses"}>
-        <p className="dark:text-slate-400">Cursos</p>
+        <p className="dark:text-slate-400 dark:hover:text-slate-200 hover:text-gray-700">
+          Cursos
+        </p>
       </Link>
       {session?.user ? (
         <>
           <Link href={"/dashboard"}>
-            <p className="dark:text-slate-400">Dashboard</p>
+            <p className="dark:text-slate-400 dark:hover:text-slate-200 hover:text-gray-700">
+              Dashboard
+            </p>
           </Link>
           <button onClick={() => signOut()}>
-            <p className="dark:text-slate-400">Cerrar Sesión</p>
+            <p className="dark:text-slate-400 dark:hover:text-slate-200 hover:text-gray-700">
+              Cerrar Sesión
+            </p>
           </button>
         </>
       ) : (
         <>
           <Link href={"/accounts/login"}>
-            <p className="dark:text-slate-400 inline-block min-w-fit whitespace-nowrap">
+            <p className="dark:text-slate-400 inline-block min-w-fit whitespace-nowrap dark:hover:text-slate-200 hover:text-gray-700">
               Iniciar Sesión
             </p>
           </Link>
           <Link href={"/accounts/register"}>
-            <p className="dark:text-slate-400">Registrarse</p>
+            <p className="dark:text-slate-400 dark:hover:text-slate-200 hover:text-gray-700">
+              Registrarse
+            </p>
           </Link>
         </>
       )}
